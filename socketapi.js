b71bcea9b04fcdc6a20b8e15396adf51f1bcb682
paras18csu153 <paras18csu153@ncuindia.edu>
2022-02-08 12:18:13 +0530

    Added Send/Receive Message.

--- socketapi.js
+++ socketapi.js
@@ -11,6 +11,10 @@ const socketapi = {
 // Add your socket.io logic here!
 io.on("connection", function (socket) {
   console.log("A user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    socket.emit("new_msg", { message: msg });
+  });
 });
 // end of socket.io logic
 
