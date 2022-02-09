f4f07e7aeb1b6a9a9334cbb940cd098b0481db75
paras18csu153 <paras18csu153@ncuindia.edu>
2022-02-09 20:40:06 +0530

    Fixed Bug.

--- socketapi.js
+++ socketapi.js
@@ -15,27 +15,13 @@ io.on("connection", function (socket) {
   console.log("A user connected");
 
   socket.on("setSocketData", (data) => {
-    data = {
-      username: data.username,
-      socket: socket,
-    };
     sockets.push(data);
   });
 
   socket.on("sendMessage", (msg) => {
-    var sendToSocket = sockets.filter(function (el) {
-      return el.username == msg.to;
-    })[0].socket;
-
-    // for (var i = 0; i < sockets.length; i++) {
-    //   if (sockets[i].username == msg.to) {
-    //     sendToSocket = sockets[i].socket;
-    //     break;
-    //   }
-    // }
-
-    if (sendToSocket) {
-      sendToSocket.emit("receiveMessage", msg);
+    if (sockets && sockets != undefined) {
+      let socketId = sockets.find((o) => o.username === msg.to).socketId;
+      socket.to(socketId).emit("receiveMessage", msg);
     }
   });
 });
